68b771c02a5e6c1457e1f11e00de56cc4b48dc5e
Juven Lawrinsen <juvenlauwrinsen7@gmai.com>
2026-02-13 00:20:57 +0700

    tes

--- Main.java
+++ Main.java
@@ -6,9 +6,21 @@ Program untuk variable
 
 public class p022WorkingVariable {
   public static void main(String[] args) {
-    int angka = 10;
-    System.out.println("Nilai awal angka: " + angka);
-    angka = 25;
-    System.out.println("Nilai angka setelah dirubah: " + angka);
-  }
+    int umur = 20;
+    long populasi = 000000000L;
+
+    float tinggi = 170.5f;
+    double ipk = 3.75
+
+    char nilaiHuruf = 'A';
+    
+    boolean lulus = true;
+    System.err.println("Umur          : " + umur);
+    System.err.println("Populasi      : " + umur);
+    System.err.println("Tinggi Badan  : " + umur);
+    System.err.println("IPK           : " + umur);
+    System.err.println("Nilai Huruf   : " + umur);
+    System.err.println("Status Lulus  : " + umur);
+
+
 }
