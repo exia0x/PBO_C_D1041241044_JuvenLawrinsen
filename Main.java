535e9e469ce9b6b261301b903c53ab8ffd0f151c
Juven Lawrinsen <juvenlauwrinsen7@gmai.com>
2026-02-12 16:37:41 +0700

    latihan pertama

---
+++ Main.java
@@ -0,0 +1,14 @@
+/*
+NIM : D1041241044
+NAMA : Juven Lawrinsen
+Program untuk variable
+*/
+
+public class p022WorkingVariable {
+  public static void main(String[] args) {
+    int angka = 10;
+    System.out.println("Nilai awal angka: " + angka);
+    angka = 25;
+    System.out.println("Nilai angka setelah dirubah: " + angka);
+  }
+}
